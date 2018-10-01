4c43c21bf133a2704f8b93726782418ab84d792b
Xraigor <43266161+Xraigor@users.noreply.github.com>
2018-10-01 11:31:19 +0200

    String htmlText = "<h1>Hello World!!</h1>";

--- src/main/java/gokan/ekinci/App.java
+++ src/main/java/gokan/ekinci/App.java
@@ -3,7 +3,7 @@ import org.jsoup.Jsoup;
 
 public class App {
     public static void main( String[] args ) {
-        String htmlText = "<h1>Hello World!</h1>";
+        String htmlText = "<h1>Hello World!!</h1>";
         System.out.println( Jsoup.parse(htmlText).text() );
     }
 }
